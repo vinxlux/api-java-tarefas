199862b9b8ef395b8089911f68746e920f676ce5
vinxlux <vinx.lux@gmail.com>
2025-11-23 20:57:20 -0300

    Atualizações finais e ajustes de deploy/teste de API

--- src/main/java/br/com/fiap/config/CORSFilter.java
+++ src/main/java/br/com/fiap/config/CORSFilter.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }

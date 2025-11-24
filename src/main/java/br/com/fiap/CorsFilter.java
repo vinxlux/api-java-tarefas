c542618d970b7f935bd8b3763dcd1c878803246b
vinxlux <vinx.lux@gmail.com>
2025-11-23 21:40:55 -0300

    Move filtro CORS para o pacote raiz e garante compatibilidade máxima com Quarkus RESTEasy

---
+++ src/main/java/br/com/fiap/CorsFilter.java
@@ -0,0 +1,19 @@
+
+package br.com.fiap;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+    }
+}
--- src/main/java/br/com/fiap/config/CORSFilter.java
+++ src/main/java/br/com/fiap/config/CORSFilter.java
@@ -1,18 +1,3 @@
 package br.com.fiap.config;
 
-import jakarta.ws.rs.container.ContainerRequestContext;
-import jakarta.ws.rs.container.ContainerResponseContext;
-import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.ext.Provider;
-import java.io.IOException;
 
-@Provider
-public class CORSFilter implements ContainerResponseFilter {
-    @Override
-    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        System.out.println("[CORSFilter] Executando filtro CORS para: " + request.getUriInfo().getRequestUri());
-        response.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    }
-}
